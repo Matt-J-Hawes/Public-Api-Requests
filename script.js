ed4cdf08b984c0284065f8e65a9fad6eaa6f1abf
Matt Hawes <73285477+Matt-J-Hawes@users.noreply.github.com>
2020-11-21 22:54:46 +0000

    Bug fixes and CSS updates

--- script.js
+++ script.js
@@ -17,29 +17,15 @@ modalSelection.className = 'modal-container'
 const errorMessage = document.createElement('h1')
 
 
-
 //FETCH API AND PARSE IT TO JSON - SUCCESS : FAILURE
 fetch('https://randomuser.me/api/?results=12')
 .then(response => response.json())
 .then(data => {
-
 	generateHTML(data.results)
     generateModal(data.results)
-
-
-    let modal = modalSelection.querySelectorAll('.modal')
-    let cards = document.querySelectorAll('.card')
-    
-
-    for(let x = 0; x < cards.length; x++){
-    	cards[x].addEventListener('click', function(){
-    		gallerySection.appendChild(modalSelection)
-    	})
-    }
 })
-
-
 .catch(systemFail)
+
 
 //FUNCTION TO DISPLAY ERROR MESSAGE IF THERE IS A SIGNAL PROBLEM 
 //FUNCTION CREATED EXTERNALLY FOR POSSIBLE REUSE
@@ -47,6 +33,7 @@ function systemFail(error){
 	gallerySection.appendChild(errorMessage)
 	errorMessage.innerHTML = "Trouble connecting with API! Please try again later."
 	console.log(error, "Trouble connecting with API") }
+
 
 //IF API IS SUCCESSFULLY FETCHED - THIS FUNCTION DISPLAYS IT TO THE PAGE
 function generateHTML(data){
@@ -62,49 +49,38 @@ const users = data.map(user =>
 			<p class="card-text cap">${user.location.city}, ${user.location.state}</p>
 		</div></div>`)
 
-gallerySection.insertAdjacentHTML('beforeend', users.join('')) 
+gallerySection.insertAdjacentHTML('beforeend', users.join('')) }
 
-}
 
 //CREATE MODAL WINDOWS (HIDDEN BY DEFAULT)
+//ITERATE OVER CARDS AND DISPLAY MODAL WINDOW IF CARD MATCHES
 function generateModal(data){
-     const html = data.map(person => 
-
-	       `
-	       <div class="modal">
+	let modal = modalSelection.querySelectorAll('.modal')
+    let cards = document.querySelectorAll('.card')
+    
+    for(let x = 0; x < cards.length; x++){
+    	cards[x].addEventListener('click', function(e){
+    		if(cards[x].html === data.innerHTML){
+    			body.appendChild(modalSelection)
+    			modalSelection.insertAdjacentHTML('beforeend', 		       
+	       `<div class="modal">
 	            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
 	            <div class="modal-info-container">
-	                <img class="modal-img" src="${person.picture.large}" alt="profile picture">
-	                <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
-	                <p class="modal-text">${person.email}</p>
-	                <p class="modal-text cap">${person.location.city}</p>
+	                <img class="modal-img" src="${data[x].picture.large}" alt="profile picture">
+	                <h3 id="name" class="modal-name cap">${data[x].name.first} ${data[x].name.last}</h3>
+	                <p class="modal-text">${data[x].email}</p>
+	                <p class="modal-text cap">${data[x].location.city}</p>
 	                <hr>
-	                <p class="modal-text">${person.phone}</p>
-	                <p class="modal-text">${person.location.street.number} ${person.location.street.name}, ${person.location.city} OR ${person.location.postcode}</p>
-	                <p class="modal-text">Birthday: ${person.dob.date.slice(5,7)}-${person.dob.date.slice(8,10)}-${person.dob.date.slice(0,4)}</p>
+	                <p class="modal-text">${data[x].phone}</p>
+	                <p class="modal-text">${data[x].location.street.number} ${data[x].location.street.name}, ${data[x].location.city} OR ${data[x].location.postcode}</p>
+	                <p class="modal-text">Birthday: ${data[x].dob.date.slice(5,7)}-${data[x].dob.date.slice(8,10)}-${data[x].dob.date.slice(0,4)}</p>
 	            </div>
-	        </div>`)
-
-     modalSelection.insertAdjacentHTML('beforeend', html)
-     
-    }
+	        </div>`)   }})}}
 
 
 //EVENT-LISTENER TO REMOVE MODEL WINDOW 
+//REMOVE HTML TO CORRECTLY DISPLAY THE NEXT USER ON CLICK
 modalSelection.addEventListener('click', function(e){
-	gallerySection.removeChild(modalSelection)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
+	body.removeChild(modalSelection)
+	modalSelection.innerHTML = '' })
 
