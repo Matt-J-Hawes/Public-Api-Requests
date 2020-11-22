d4adf68d3936163f8fa9e62bc3641b0820b56c98
Matt Hawes <73285477+Matt-J-Hawes@users.noreply.github.com>
2020-11-22 16:23:21 +0000

    Format phone number correctly

--- script.js
+++ script.js
@@ -14,7 +14,13 @@ const body = document.querySelector('body')
 const errorMessage = document.createElement('h1')
 const gallerySection = document.getElementById('gallery')
 const modalSelection = document.createElement('div')
+const buttonPrev = document.createElement('button')
+const buttonNext = document.createElement('button')
+buttonPrev.className = 'modal-prev'
+buttonNext.className = 'modal-next'
 modalSelection.className = 'modal-container'
+buttonPrev.innerHTML = "Prev"
+buttonNext.innerHTML = "Next"
 
 
 //FETCH API AND PARSE IT TO JSON - SUCCESS : FAILURE
@@ -59,21 +65,32 @@ function generateModal(data){
     for(let x = 0; x < cards.length; x++){
     	cards[x].addEventListener('click', function(e){
     		if(cards[x].html === data.innerHTML){
-    			body.appendChild(modalSelection)
-    			modalSelection.insertAdjacentHTML('beforeend', 		       
-	       `<div class="modal">
-	            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-	            <div class="modal-info-container">
-	                <img class="modal-img" src="${data[x].picture.large}" alt="profile picture">
-	                <h3 id="name" class="modal-name cap">${data[x].name.first} ${data[x].name.last}</h3>
-	                <p class="modal-text">${data[x].email}</p>
-	                <p class="modal-text cap">${data[x].location.city}</p>
-	                <hr>
-	                <p class="modal-text">${data[x].phone}</p>
-	                <p class="modal-text">${data[x].location.street.number} ${data[x].location.street.name}, ${data[x].location.city} ${data[x].location.postcode}</p>
-	                <p class="modal-text">Birthday: ${data[x].dob.date.slice(5,7)}/${data[x].dob.date.slice(8,10)}/${data[x].dob.date.slice(2,4)}</p>
-	            </div>
-	        </div>`)   }})}}
+
+	//FORMAT PHONE NUMBER CORRECTLY - (XXX) XXX-XXXX
+	const phoneReplace = data[x].phone.split('').filter(y => y!== '-' && y!== '(' && y !== ')').join('')
+	const phoneOne = `(${phoneReplace.slice(0,3)})`
+	const phoneTwo = `${phoneReplace.slice(4-7)}`
+	const phoneThree = `${phoneReplace.slice(8-12)}`
+	const phone = `${phoneOne} ${phoneTwo}-${phoneThree}`
+
+		body.appendChild(modalSelection)
+		modalSelection.insertAdjacentHTML('beforeend', 		       
+       `<div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${data[x].picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${data[x].name.first} ${data[x].name.last}</h3>
+                <p class="modal-text">${data[x].email}</p>
+                <p class="modal-text cap">${data[x].location.city}</p>
+                <hr>
+                <p class="modal-text">${phone}</p>
+                <p class="modal-text">${data[x].location.street.number} ${data[x].location.street.name}, ${data[x].location.city} ${data[x].location.postcode}</p>
+                <p class="modal-text">Birthday: ${data[x].dob.date.slice(5,7)}/${data[x].dob.date.slice(8,10)}/${data[x].dob.date.slice(2,4)}</p>
+            </div></div>`)  
+	 modalSelection.appendChild(buttonPrev)
+	 modalSelection.appendChild(buttonNext) 
+        
+     }})}}
 
 
 //EVENT-LISTENER TO REMOVE MODEL WINDOW 
